16db96b8f8502043dcdc986c97f2caae578a892a
Lewis <Lewis@DESKTOP-FAOVK91>
2021-11-17 13:54:52 +1000

    Changed config to enable swagger in production

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())       // remove is production to prevent swagger ui in published version
 {
     app.UseSwagger();
     app.UseSwaggerUI();
